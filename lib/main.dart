7b96b106411524c587271d9d5a39d6947ed5b522
shhad2021 <79218579+shhad2021@users.noreply.github.com>
2021-03-24 00:31:12 +0300

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Ali extends StatelessWidget {
+class yfjyf extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container();
